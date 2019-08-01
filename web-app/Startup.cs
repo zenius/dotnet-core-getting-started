4fa49913c08c42712764e37720d2c6871d5587c4
zenius <zenius2050@gmail.com>
2019-08-01 19:19:01 +0530

    index.cshtml page added

--- web-app/Startup.cs
+++ web-app/Startup.cs
@@ -15,6 +15,7 @@ namespace web_app
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,8 @@ namespace web_app
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+            
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
